9f2637d781aae1b8069ac3765eb30c5ad8ccff96
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-10-21 10:40:07 -0400

    Initial commit static html styling

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/entry.jsx',
+  output: {
+    filename: './app/assets/javascript/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
