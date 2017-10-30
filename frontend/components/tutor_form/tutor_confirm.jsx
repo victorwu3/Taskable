b78938a0688d1fe6a8ea84f3b9de31bdaac9ee92
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-10-30 18:01:18 -0400

    update promise for confirming booking

--- frontend/components/tutor_form/tutor_confirm.jsx
+++ frontend/components/tutor_form/tutor_confirm.jsx
@@ -42,8 +42,8 @@ class TutorConfirm extends React.Component {
       location: localStorage.getItem('address')
     };
     this.props.createBooking(params).then(
-      this.props.fetchHiredTutors(this.props.currentUser.id).then(this.props.history.push('/dashboard'))
-    );
+      this.props.fetchHiredTutors(this.props.currentUser.id)).then(this.props.history.push('/dashboard'))
+    ;
   }
 
   render(){
