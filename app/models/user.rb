bb5a4218f1077f927d1c0ba3e67bc5dae23e6769
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-10-30 11:52:20 -0400

    bucket default pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true}
 
-  has_attached_file :image, default_url: ("default.jpeg")
+  has_attached_file :image, default_url: ("https://s3.us-east-2.amazonaws.com/app-taskable-pro/default.jpeg")
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   after_initialize :ensure_session_token
