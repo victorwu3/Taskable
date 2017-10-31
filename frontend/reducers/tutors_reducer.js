1ca254c23211d5b499ac565dfa063710d89d2372
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-10-30 23:08:57 -0400

    fix hiredtutor reducer for logging in diff user

--- frontend/reducers/tutors_reducer.js
+++ frontend/reducers/tutors_reducer.js
@@ -1,3 +1,4 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_TUTORS, RECEIVE_HIRED_TUTORS } from '../actions/tutor_actions';
 import merge from 'lodash/merge';
 
@@ -10,6 +11,8 @@ const TutorsReducer = (state = defaultState, action) => {
       return merge({}, state, {params: action.params}, {tutors: action.tutors});
     case RECEIVE_HIRED_TUTORS:
       return merge({}, state, {hiredTutors: action.hiredTutors});
+    case RECEIVE_CURRENT_USER:
+      return { hiredTutors: [] };
     default:
       return state;
   }
