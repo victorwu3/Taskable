e2dbfa7c326b6eb3ad46fc7ddb6221dca1ffc411
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-11-08 12:08:45 -0500

    env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,26 @@
 var path = require('path');
-var InlineEnvironmentVariablesPlugin = require('inline-environment-variables-webpack-plugin');
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
 
 module.exports = {
   entry: './frontend/taskable.jsx',
@@ -21,8 +42,5 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
-  },
-  plugins: [
-    new InlineEnvironmentVariablesPlugin()
-  ]
+  }
 };
