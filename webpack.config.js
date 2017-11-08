b48c9360cf58736c0030ad397f8e33060ad136f4
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-11-08 11:53:26 -0500

    remove env

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,5 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-  ]
+  }
 };
