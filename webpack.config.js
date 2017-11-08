bda116655466595a09fd6128dce8a77e86141479
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-11-08 11:51:26 -0500

    production env

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
   ]
 };
